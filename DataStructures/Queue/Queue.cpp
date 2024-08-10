54b3f7d1f80cc8fee238087ecf991fd585e53381
Gabriel Felipe Quaresma de Oliveira <78746270+GabrielFQuaresma@users.noreply.github.com>
2024-08-10 22:09:50 +0000

    New Queue class that uses the linkedlist methods

---
+++ DataStructures/Queue/Queue.cpp
@@ -0,0 +1,34 @@
+#include "../List/LinkedList.cpp"
+template <typename T>
+
+class Queue {
+private:
+    LinkedList<T> list; // Não tenho certeza se precisa ser ponteiro
+
+public:
+    Queue() = default;
+
+    void enqueue(T value) {
+        list.pushBack(value);
+    }
+
+    T dequeue() {
+        if (isEmpty()) throw std::runtime_error("Error: Empty Queue.");
+        return list.popFront();
+    }
+
+    bool search (T value) const {
+        if (isEmpty()) throw std::runtime_error("Error: Empty Queue.");
+        return list.search(value);
+    }
+
+    int size() const {
+        return list.size();
+    }
+
+    bool isEmpty() const {
+        return size() == 0;
+    }
+
+    void printQueue() const { list.printList(); }
+};
